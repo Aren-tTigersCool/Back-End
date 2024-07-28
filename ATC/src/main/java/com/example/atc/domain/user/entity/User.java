ff8d2c40b7381276c1a153afc5fff66168530606
MoonInbae <mib010213@gmail.com>
2024-07-29 00:41:22 +0900

    FEAT: User PointRecord 관계 구현
    - pointRecord 연결

--- ATC/src/main/java/com/example/atc/domain/user/entity/User.java
+++ ATC/src/main/java/com/example/atc/domain/user/entity/User.java
@@ -1,7 +1,12 @@
 package com.example.atc.domain.user.entity;
 
+import com.example.atc.domain.pointRecord.entity.PointRecord;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.Data;
+
+import java.util.LinkedList;
+import java.util.List;
 
 @Data
 @Entity
@@ -19,4 +24,8 @@ public class User {
     private Double calSum;
     private Double carSum;
     private int totalPoint;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference
+    private List<PointRecord> pointRecords = new LinkedList<>();
 }
