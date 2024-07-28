8c5282cec85aaa00b2a7ec0bd1785ad627679017
MoonInbae <mib010213@gmail.com>
2024-07-29 01:32:09 +0900

    REFACTOR: Plogging 메소드 이름 변경

--- ATC/src/main/java/com/example/atc/domain/plogging/controller/PloggingController.java
+++ ATC/src/main/java/com/example/atc/domain/plogging/controller/PloggingController.java
@@ -18,19 +18,19 @@ public class PloggingController {
 
     @Operation(summary = "플로깅 내역 조회", description = "전체 플로깅 내역을 조회합니다.")
     @GetMapping
-    public List<Plogging> getAllPosts() {
+    public List<Plogging> getAllPloggings() {
         return ploggingService.retrieveAllPloggings();
     }
 
     @Operation(summary = "플로깅 인증 내역 생성", description = "플로깅 인증 내역을 생성합니다.")
     @PostMapping
-    public Plogging createPost(@RequestBody Plogging plogging) {
+    public Plogging createPlogging(@RequestBody Plogging plogging) {
         return ploggingService.createPlogging(plogging);
     }
 
     @Operation(summary = "게시물 수정", description = "게시물을 수정합니다.")
     @PutMapping("/{recordId}")
-    public void updatePost(@PathVariable Long recordId, @RequestBody Plogging ploggingDetails) {
+    public void updatePlogging(@PathVariable Long recordId, @RequestBody Plogging ploggingDetails) {
         ploggingService.updatePlogging(recordId, ploggingDetails);
     }
 
