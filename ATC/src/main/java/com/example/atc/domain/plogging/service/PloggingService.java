14068c800931dcce10d9805bf94cca4c841ad19b
BEOMJIN LEE <beomjin754@gmail.com>
2024-07-28 21:15:43 +0900

    FEAT:PicURL to DB

--- ATC/src/main/java/com/example/atc/domain/plogging/service/PloggingService.java
+++ ATC/src/main/java/com/example/atc/domain/plogging/service/PloggingService.java
@@ -1,15 +1,14 @@
 package com.example.atc.domain.plogging.service;
 
-import com.amazonaws.services.s3.AmazonS3;
 import com.example.atc.domain.plogging.entity.Plogging;
+import com.example.atc.domain.plogging.entity.PloggingPicture;
+import com.example.atc.domain.plogging.repository.PloggingPictureRepository;
 import com.example.atc.domain.plogging.repository.PloggingRepository;
 import com.example.atc.global.S3UploadService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
@@ -19,6 +18,10 @@ public class PloggingService {
     private PloggingRepository ploggingRepository;
 
     @Autowired
+    private PloggingPictureRepository ploggingPictureRepository;
+
+
+    @Autowired
     private S3UploadService s3UploadService;
     public List<Plogging> retrieveAllPloggings(){return ploggingRepository.findAll();}
 
@@ -26,7 +29,7 @@ public class PloggingService {
         return ploggingRepository.save(plogging);
     }
 
-    public void upadatePlogging(Long recordId, Plogging ploggingDetails){
+    public void updatePlogging(Long recordId, Plogging ploggingDetails){
         Plogging plogging = ploggingRepository.findById(recordId).orElseThrow(()->new IllegalArgumentException("Invalid post ID: "+recordId));
         plogging.setTimeTaken(ploggingDetails.getTimeTaken());
         plogging.setDistance(ploggingDetails.getDistance());
@@ -38,7 +41,11 @@ public class PloggingService {
         ploggingRepository.deleteById(recordId);
     }
 
-    public String uploadFile(MultipartFile file) throws IOException {
-        return s3UploadService.saveFile(file);
+
+    public PloggingPicture savePicture(MultipartFile file) throws IOException{
+        String pictureUrl = s3UploadService.saveFile(file);
+        PloggingPicture ploggingPicture = new PloggingPicture();
+        ploggingPicture.setPictureUrl(pictureUrl);
+        return ploggingPictureRepository.save(ploggingPicture);
     }
 }
