28e8df1257b53ea18d0af9794cd2068e20924a69
Inseo-Baek <inseobaek625@gmail.com>
2024-08-04 16:18:15 +0900

    FEAT:전체 포인트와 오늘전체 포인트 기능 추가

--- ATC/src/main/java/com/example/atc/domain/pointRecord/repository/PointRecordRepository.java
+++ ATC/src/main/java/com/example/atc/domain/pointRecord/repository/PointRecordRepository.java
@@ -3,5 +3,8 @@ package com.example.atc.domain.pointRecord.repository;
 import com.example.atc.domain.pointRecord.entity.PointRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PointRecordRepository extends JpaRepository<PointRecord, Long> {
+    List<PointRecord> findAllByUserAndUsedDate(User user, String usedDate);
 }
