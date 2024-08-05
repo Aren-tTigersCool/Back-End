0478c132779bc8e53976c277dc6b33287eccb6d4
D <islan_d@naver.com>
2024-08-05 20:47:41 +0900

    Fix: 회원가입 수정

--- ATC/src/main/java/com/example/atc/domain/user/controller/UserController.java
+++ ATC/src/main/java/com/example/atc/domain/user/controller/UserController.java
@@ -1,15 +1,13 @@
 package com.example.atc.domain.user.controller;
 
 import com.example.atc.domain.user.dto.UserDTO;
+import com.example.atc.domain.user.dto.idDTO;
 import com.example.atc.domain.user.dto.logInDTO;
 import com.example.atc.domain.user.dto.signUpDTO;
 import com.example.atc.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
@@ -18,25 +16,25 @@ public class UserController {
 
     private final UserService userService;
     @PostMapping("/check")
-    public boolean checkIdAndPw(@RequestBody String memberId) {
-        return userService.checkId(memberId);
+    public boolean checkIdAndPw(@RequestBody idDTO idDTO) {
+        return userService.checkId(idDTO.getMemberId());
     }
 
     @PostMapping("/signUp")
     public ResponseEntity<?> signUp(@RequestBody signUpDTO signUpDTO) {
-        if (userService.checkId(signUpDTO.getMemberId())){
+        if (!userService.checkId(signUpDTO.getMemberId())){
             userService.signUp(signUpDTO);
             return ResponseEntity.ok("회원가입 성공");
         }
-        else return ResponseEntity.ok("회원가입 실패");
+        else return ResponseEntity.ok("회원가입 실패입니다.");
     }
     @PostMapping ("/login") //로그인
     public ResponseEntity<?> login(@RequestBody logInDTO logInDTO) {
         return userService.login(logInDTO);
     }
     @PostMapping
-    public ResponseEntity<?> createUser(@RequestPart UserDTO userDTO, @RequestPart MultipartFile file) {
-        return userService.createUser(userDTO, file);
+    public ResponseEntity<?> createUser(@RequestPart UserDTO userDTO) {
+        return userService.createUser(userDTO);
     }
 
     @GetMapping
@@ -49,11 +47,15 @@ public class UserController {
         return userService.getUserById(id);
     }
 
-    @PutMapping("/{id}") //회원정보 수정
+    @GetMapping("/{memberId}")
+    public ResponseEntity<?> getUserByMemberId(@PathVariable String memberId) {
+        return userService.getUserByMemberId(memberId);
+    }
+
+    @PutMapping("/{id}")
     public ResponseEntity<?> updateUser(@PathVariable Long id,
-                                        @RequestPart UserDTO userDTO,
-                                        @RequestPart(required = false) MultipartFile file) {
-        return userService.updateUser(id, userDTO, file);
+                                        @RequestPart UserDTO userDTO) {
+        return userService.updateUser(id, userDTO);
     }
 
     @DeleteMapping("/{id}")
--- ATC/src/main/java/com/example/atc/domain/user/service/UserService.java
+++ ATC/src/main/java/com/example/atc/domain/user/service/UserService.java
@@ -1,25 +1,22 @@
 package com.example.atc.domain.user.service;
 
-import com.example.atc.domain.pointRecord.entity.PointRecord;
 import com.example.atc.domain.user.dto.UserDTO;
 import com.example.atc.domain.user.dto.logInDTO;
 import com.example.atc.domain.user.dto.signUpDTO;
-import com.example.atc.domain.user.entity.ProfilePicture;
 import com.example.atc.domain.user.entity.User;
 import com.example.atc.domain.user.repository.ProfilePictureRepository;
 import com.example.atc.domain.user.repository.UserRepository;
 import com.example.atc.global.S3UploadService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
+
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
-import java.io.IOException;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -60,7 +57,7 @@ public class UserService {
     }
 
     public boolean checkId(String memberId) {
-        return !userRepository.existsByMemberId(memberId);
+        return userRepository.existsByMemberId(memberId);
     }
 
     public ResponseEntity<?> signUp(signUpDTO signUpDTO) {
@@ -101,32 +98,6 @@ public class UserService {
         }
     }
 
-//    public boolean isAvailable(Long userId, String pw) {
-//        return !userRepository.existsByUserIdAndUserPw(userId, pw );
-//    }
-
-//    public ResponseEntity<?> signUp(UserDTO userDTO) {
-//        try {
-//            if (userRepository.existsById(userDTO.getUserId())) {
-//                return ResponseEntity.status(HttpStatus.CONFLICT).body("이미 존재하는 ID입니다.");
-//            }
-//
-//            String pw = userDTO.getUserPw();
-//            if (!isValidPassword(String.valueOf(pw))) {
-//                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("비밀번호는 8자 이상, 대문자, 소문자, 숫자 및 특수문자를 포함해야 합니다.");
-//            }
-//
-//            User user = new User();
-//            User changedUser = this.setEntityData(userDTO, user);
-//
-//            User savedUser = userRepository.save(changedUser);
-//
-//            return ResponseEntity.status(HttpStatus.OK).body(savedUser);
-//        } catch (Exception e) {
-//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("알 수 없는 오류가 발생했습니다.");
-//        }
-//    }
-
     // 비밀번호 검증
     private boolean isValidPassword(String password) {
         if (password == null) {
@@ -140,25 +111,13 @@ public class UserService {
     }
 
 
-    public ResponseEntity<?> createUser(UserDTO userDTO, MultipartFile file) {
-        if (file == null || file.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("업로드 할 이미지가 존재하지 않습니다.");
-        }
-        String fileUrl;
+    public ResponseEntity<?> createUser(UserDTO userDTO) {
         try {
-            fileUrl = savePicture(file);
-            ProfilePicture profilePicture = new ProfilePicture();
-            profilePicture.setPictureUrl(fileUrl);
-
             User user = new User();
             User changedUser = this.setEntityData(userDTO, user);
-            changedUser.setProfilePicture(profilePicture);
-            profilePicture.setUser(changedUser);
-
-            profilePictureRepository.save(profilePicture);
             return ResponseEntity.status(HttpStatus.OK).body(userRepository.save(changedUser));
-        } catch (IOException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버 문제로 S3 이미지 업로드에 실패하였습니다.");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버 연결에 실패하였습니다.");
         }
     }
 
@@ -173,25 +132,19 @@ public class UserService {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found with id " + userId);
     }
 
-    public ResponseEntity<?> updateUser(Long userId, UserDTO userDTO, MultipartFile file) {
+    public ResponseEntity<?> getUserByMemberId(String memberId){
+        Optional<User> user = userRepository.findByMemberId(memberId);
+        if (user.isPresent())
+            return ResponseEntity.status(HttpStatus.OK).body(user.get());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found with id " + memberId);
+    }
+
+    public ResponseEntity<?> updateUser(Long userId, UserDTO userDTO) {
         Optional<User> userOptional = userRepository.findById(userId);
         if (userOptional.isPresent()) {
             User user = userOptional.get();
             User changedUser = this.setEntityData(userDTO, user);
 
-            ProfilePicture profilePicture = user.getProfilePicture() != null ? user.getProfilePicture() : new ProfilePicture();
-            if (file != null && !file.isEmpty()) {
-                try {
-                    String fileUrl = savePicture(file);
-                    profilePicture.setPictureUrl(fileUrl);
-                } catch (IOException e) {
-                    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버 문제로 S3 이미지 업로드에 실패하였습니다.");
-                }
-            }
-            profilePicture.setUser(changedUser);
-            changedUser.setProfilePicture(profilePicture);
-
-            profilePictureRepository.save(profilePicture);
             return ResponseEntity.status(HttpStatus.OK).body(userRepository.save(changedUser));
         } else {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found with id " + userId);
@@ -201,10 +154,6 @@ public class UserService {
     public ResponseEntity<?> deleteUser(Long userId) {
         Optional<User> userOptional = userRepository.findById(userId);
         if (userOptional.isPresent()) {
-            ProfilePicture profilePicture = userOptional.get().getProfilePicture();
-            if (profilePicture != null) {
-                profilePictureRepository.delete(profilePicture);
-            }
             userRepository.deleteById(userId);
             return ResponseEntity.status(HttpStatus.OK).body("User deleted successfully");
         }
@@ -221,55 +170,12 @@ public class UserService {
         user.setCalSum(userDTO.getCalSum());
         user.setCarSum(userDTO.getCarSum());
         user.setTotalPoint(userDTO.getTotalPoint());
-
         return user;
     }
 
-    private String savePicture(MultipartFile file) throws IOException {
-        return s3UploadService.saveFile(file);
-    }
 
     public ResponseEntity<User> saveUser(User user){
         user = userRepository.save(user);
         return ResponseEntity.status(HttpStatus.OK).body(user);
     }
 }
-
-
-
-//    public User registerKakaoUser(UserDTO userDTO) {
-//        User kakaoUser = userRepository.findById(userDTO.getUserId())
-//                .orElse(null);
-//
-//        ProfilePicture kakaoProfilePicture;
-//
-//        if (kakaoUser != null) { // 카카오 사용자 email 동일한 email 가진 회원이 있는지 확인
-//            // 기존 프로필 사진 객체를 재사용
-//            kakaoProfilePicture = kakaoUser.getProfilePicture() != null ? kakaoUser.getProfilePicture() : new ProfilePicture();
-//            kakaoUser.setUserId(kakaoId);
-//            kakaoUser.setEmail(email);
-//            //닉네임도 변경 가능 여부 (카카오랑 다르게)에 따라 이부분 바뀜
-//            kakaoUser.setNickName(nickname);
-//
-//            System.out.println(nickname + "님 로그인");
-//        } else {
-//            kakaoProfilePicture = new ProfilePicture();
-//
-//            kakaoUser = User.builder()
-//                    .userId(kakaoId)
-//                    .nickName(nickname)
-//                    .email(email)
-//                    .profilePicture(kakaoProfilePicture)
-//                    .build();
-//
-//            System.out.println(nickname + "님 회원가입");
-//        }
-//
-//        kakaoProfilePicture.setPictureUrl(imageUrl); //카카오프사랑 늘 같은 프사일 때
-//        kakaoProfilePicture.setUser(kakaoUser);
-//
-//        profilePictureRepository.save(kakaoProfilePicture);
-//        userRepository.save(kakaoUser); // 변경사항을 저장
-//
-//        return kakaoUser;
-//    }
