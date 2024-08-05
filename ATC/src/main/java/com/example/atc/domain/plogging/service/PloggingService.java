8532c4b4590a0fda6babda78f9ea11af7ace3286
BEOMJIN LEE <beomjin754@gmail.com>
2024-08-05 17:13:58 +0900

    Fix:conflict

--- ATC/src/main/java/com/example/atc/domain/plogging/service/PloggingService.java
+++ ATC/src/main/java/com/example/atc/domain/plogging/service/PloggingService.java
@@ -32,31 +32,31 @@ public class PloggingService {
     private final S3UploadService s3UploadService;
     public List<Plogging> retrieveAllPloggings(){return ploggingRepository.findAll();}
 
-    public ResponseEntity<?> createPlogging(PloggingDto dto, MultipartFile file) {
-        if(file == null || file.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("업로드 할 이미지가 존재하지 않습니다.");
-        }
-        String fileUrl = null;
-        try {
-            fileUrl = savePicture(file);
-            PloggingPicture ploggingPicture = new PloggingPicture();
-            ploggingPicture.setPictureUrl(fileUrl);
-
-            Plogging plogging = Plogging.builder()
-                    .timeTaken(dto.getTimeTaken())
-                    .authenticationTime(dto.getAuthenticationTime())
-                    .distance(dto.getDistance())
-                    .location(dto.getLocation())
-                    .ploggingPicture(ploggingPicture)
-                    .build();
-
-            ploggingPicture.setPlogging(plogging);
-            ploggingPictureRepository.save(ploggingPicture);
-            return ResponseEntity.ok().body(ploggingRepository.save(plogging));
-        } catch (IOException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버 문제로 S3 이미지 업로드에 실패하였습니다.");
-        }
-    }
+//    public ResponseEntity<?> createPlogging(PloggingDto dto, MultipartFile file) {
+//        if(file == null || file.isEmpty()) {
+//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("업로드 할 이미지가 존재하지 않습니다.");
+//        }
+//        String fileUrl = null;
+//        try {
+//            fileUrl = savePicture(file);
+//            PloggingPicture ploggingPicture = new PloggingPicture();
+//            ploggingPicture.setPictureUrl(fileUrl);
+//
+//            Plogging plogging = Plogging.builder()
+//                    .timeTaken(dto.getTimeTaken())
+//                    .authenticationTime(dto.getAuthenticationTime())
+//                    .distance(dto.getDistance())
+//                    .location(dto.getLocation())
+//                    .ploggingPicture(ploggingPicture)
+//                    .build();
+//
+//            ploggingPicture.setPlogging(plogging);
+//            ploggingPictureRepository.save(ploggingPicture);
+//            return ResponseEntity.ok().body(ploggingRepository.save(plogging));
+//        } catch (IOException e) {
+//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버 문제로 S3 이미지 업로드에 실패하였습니다.");
+//        }
+//    }
 
     public void updatePlogging(Long recordId, Plogging ploggingDetails){
         Plogging plogging = ploggingRepository.findById(recordId).orElseThrow(()->new IllegalArgumentException("Invalid post ID: "+recordId));
@@ -71,13 +71,13 @@ public class PloggingService {
     }
 
 
-//    public PloggingPicture savePicture(MultipartFile file) throws IOException{
-//        String pictureUrl = s3UploadService.saveFile(file);
-//        PloggingPicture ploggingPicture = new PloggingPicture();
-//        ploggingPicture.setPictureUrl(pictureUrl);
-//        return ploggingPictureRepository.save(ploggingPicture);
-    //}
-    public String savePicture(MultipartFile file) throws IOException {
-        return s3UploadService.saveFile(file);
+    public PloggingPicture savePicture(MultipartFile file) throws IOException{
+        String pictureUrl = s3UploadService.saveFile(file);
+        PloggingPicture ploggingPicture = new PloggingPicture();
+        ploggingPicture.setPictureUrl(pictureUrl);
+        return ploggingPictureRepository.save(ploggingPicture);
     }
+//    public String savePicture(MultipartFile file) throws IOException {
+//        return s3UploadService.saveFile(file);
+//    }
 }
