adf4a70cc86bb936bab28aaaf1985a0fe0944605
BEOMJIN LEE <beomjin754@gmail.com>
2024-08-05 21:25:28 +0900

    FEAT:Find By Id

--- ATC/src/main/java/com/example/atc/domain/user/repository/UserRepository.java
+++ ATC/src/main/java/com/example/atc/domain/user/repository/UserRepository.java
@@ -2,14 +2,18 @@ package com.example.atc.domain.user.repository;
 
 import com.example.atc.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("SELECT u FROM User u WHERE u.memberId = :memberId")
+    Optional<User> findByMemberId(@Param("memberId") String memberId);
+
     boolean existsByMemberIdAndUserPw(String memberId, String userPw);
     boolean existsByMemberId(String memberId);
-    Optional<User> findByMemberId(String memberId);
-
 }
