4d509848f10424e931c311eb11348cd7887b2d57
BEOMJIN LEE <beomjin754@gmail.com>
2024-08-05 21:48:24 +0900

    FEAT:add Query in searchId

--- ATC/src/main/java/com/example/atc/domain/user/repository/UserRepository.java
+++ ATC/src/main/java/com/example/atc/domain/user/repository/UserRepository.java
@@ -13,7 +13,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query("SELECT u FROM User u WHERE u.memberId = :memberId")
     Optional<User> findByMemberId(@Param("memberId") String memberId);
-
-    boolean existsByMemberIdAndUserPw(String memberId, String userPw);
-    boolean existsByMemberId(String memberId);
+    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END FROM User u WHERE u.memberId = :memberId AND u.userPw = :userPw")
+    boolean existsByMemberIdAndUserPw(@Param("memberId") String memberId, @Param("userPw") String userPw);
+    @Query("SELECT u.userId FROM User u WHERE u.memberId = :memberId")
+    boolean existsByMemberId(@Param("memberId") String memberId);
 }
