0f5b21f57612cf98f6782133531ff1f3e39b9b2a
BEOMJIN LEE <beomjin754@gmail.com>
2024-08-05 21:54:46 +0900

    Refactor:delete query

--- ATC/src/main/java/com/example/atc/domain/user/repository/UserRepository.java
+++ ATC/src/main/java/com/example/atc/domain/user/repository/UserRepository.java
@@ -11,10 +11,7 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    @Query("SELECT u FROM User u WHERE u.memberId = :memberId")
     Optional<User> findByMemberId(@Param("memberId") String memberId);
-    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END FROM User u WHERE u.memberId = :memberId AND u.userPw = :userPw")
     boolean existsByMemberIdAndUserPw(@Param("memberId") String memberId, @Param("userPw") String userPw);
-    @Query("SELECT u.userId FROM User u WHERE u.memberId = :memberId")
     boolean existsByMemberId(@Param("memberId") String memberId);
 }
